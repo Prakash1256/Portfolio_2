11e629fe9ec8881c7b56af7bbc8b4de2cf167b3c
Prakash1256 <158434598+Prakash1256@users.noreply.github.com>
2024-08-09 20:00:30 +0530

    push my code

---
+++ README.md
@@ -0,0 +1,70 @@
+# React Developer Portfolio template
+#### Live Demo https://prakash1256.github.io/Portfolio/
+#### Figma Design 
+### Preview ( Dark Mode )
+
+<<<<<<< HEAD
+<!-- <img src="/preview.gif" alt="preview" width="600px" /> -->
+=======
+>>>>>>> 6185518b7a1e7655506f5cc5ffcf9e6402804264
+
+## Follow Below instructions to add your deatils in the portfolio.
+
+### Change and customize every section according to your need, All you need to do is edit `/src/Details.js`
+
+#### Open `/src/Details.js` & modify it as per your need.
+
+![Screenshot 2024-07-07 145309](https://github.com/Prakash1256/Portfolio/assets/158434598/d86b7049-3394-44e1-a4ad-4230b24b5e82)
+
+
+```javascript
+/ Enter your Personal Details here
+export const personalDetails = {
+  name: "Kunwar Prakash Singh",
+<<<<<<< HEAD
+  tagline: "Full stack devloper",
+=======
+  tagline: "Full-stack devloper",
+>>>>>>> 6185518b7a1e7655506f5cc5ffcf9e6402804264
+  img: profile,
+  about: `Kunwar Prakash Singh
+Full Stack Developer
+
+I am Kunwar Prakash Singh, a full stack developer with a strong proficiency in both front-end and back-end technologies. My technical expertise spans HTML, CSS, JavaScript, React, Redux, MongoDB, SQL, and SASS I am passionate about crafting scalable and efficient web solutions and have a proven track record of overcoming technical challenges through exploration and continuous learning...`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/",
+  github: "https://www.github.com/",
+ 
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+<<<<<<< HEAD
+    Position: "Full stack devloper",
+    Company: `Company Name here`,
+=======
+    Position: "Full-stack devloper",
+    Company: `Masai`,
+>>>>>>> 6185518b7a1e7655506f5cc5ffcf9e6402804264
+    Location: "Bengaluru",
+    Type: "Full Time",
+    Duration: "Oct 2023 - Jun 2024",
+  }
+];
+
+//  Education Details here
+export const eduDetails = { ... };
+
+// Tech Stack and Tools
+export const techStackDetails = { ... };
+
+//  Project Details here
+export const projectDetails = [ ... ];
+
+//  Contact Details here
+export const contactDetails = { ... };
+```
---
+++ src/Details.js
@@ -0,0 +1,156 @@
+// Logo images
+import logogradient from "./assets/logo.svg";
+
+// Profile Image
+import profile from "./assets/profile.png";
+
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import npm from "./assets/techstack/npm.png";
+import figma from "./assets/techstack/figma.png";
+
+// Project Images
+import projectImage1 from "./assets/projects/project1.png";
+import projectImage2 from "./assets/projects/project2.png";
+import projectImage3 from "./assets/projects/project3.png";
+import projectImage4 from "./assets/projects/project4.png";
+import projectImage5 from "./assets/projects/project5.png";
+import projectImage6 from "./assets/projects/project6.png";
+
+// Logos
+export const logos = {
+  logogradient,
+};
+
+// Personal Details 
+export const personalDetails = {
+  name: "Kunwar Prakash Singh",
+  tagline: "I'm a full-stack developer",
+  img: profile,
+  about: `Hello, I'm Kunwar Prakash Singh, a full stack developer skilled in HTML5, CSS3, MongoDB, SQL, JavaScript, React.js, and Node.js. I specialize in creating dynamic, responsive web applications with expertise in both front-end and back-end development.`,
+};
+
+// Social Media URLs 
+export const socialMediaUrl = {
+  linkedin: "https://www.linkedin.com/in/kunwar-prakash-singh-7ba04b296",
+  github: "https://github.com/Prakash1256",
+};
+
+// Work Experience 
+export const workDetails = [
+  {
+    Position: "FullStack Developer",
+    Company: "Masai",
+    Location: "Bengaluru",
+    Type: "Training",
+    Duration: "Oct 2023 - Aug 2024",
+  },
+  {
+    Position: "FullStack Developer Internship",
+    Company: "Edureka",
+    Location: "Bengaluru",
+    Type: "Training",
+    Duration: "july 2022 - january 2023",
+  },
+];
+
+// Education Details
+export const eduDetails = [
+  {
+    Position: "FullStack Developer",
+    Company: "Masai",
+    Location: "Bengaluru",
+    Type: "Training",
+    Duration: "Oct 2023 - Aug 2024",
+  },
+  {
+    Position: "DIPLOMA IN ELEMENTARY EDUCATION (D.El.Ed.)",
+    Company: "Duja Devi Mahavidyalya Sahtwar Ballia",
+    Location: "Uttar-Pradesh",
+    Type: "Full Time",
+    Duration: "Jun 2019 - May 2024",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html,
+  css,
+  js,
+  react,
+  redux,
+  sass,
+  tailwind,
+  bootstrap,
+  vscode,
+  npm,
+  github,
+  figma,
+};
+
+// Project Details 
+export const projectDetails = [
+  {
+    title: "Shine Heaven",
+    image: projectImage1,
+    description: `Shine Haven is a premium brand dedicated to providing high-quality personal care products. Our mission is to enhance your daily self-care routine with effective, gentle, and sustainable solutions that make you feel your best every day.`,
+    techstack: "HTML/CSS, JavaScript, React.js",
+    previewLink: "https://66713797ff903dc0919ec5ca--bucolic-banoffee-8c19f2.netlify.app/",
+    githubLink: "https://github.com/BabakhalilM/Huawei-Heroes_081",
+  },
+  {
+    title: "Clone of better.com",
+    image: projectImage5,
+    description: `Better.com is a digital mortgage lender and financial services company that aims to simplify the home-buying process. Founded in 2016, Better.com leverages technology to provide a fast, transparent, and user-friendly experience for obtaining mortgages, refinancing, and managing home loans.`,
+    techstack: "HTML/CSS, JavaScript, Next.js, Tailwind.css",
+    previewLink: "https://fluffy-lokum-2e9ded.netlify.app/",
+    githubLink: "https://github.com/Prakash1256/better",
+  },
+  {
+    title: "Clone of Springdale School",
+    image: projectImage6,
+    description: `Springdale School Clone is a conceptual project aimed at replicating the functionalities and features of a school management system. It provides a comprehensive solution for managing various aspects of school operations, ensuring efficient and streamlined administration.`,
+    techstack: "HTML/CSS, JavaScript, React.js, Tailwind.css",
+    previewLink: "https://66a75d1a195bb1063d3bc4c8--dreamy-vacherin-367ff1.netlify.app/",
+    githubLink: "https://github.com/Prakash1256/internship_project",
+  },
+  {
+    title: "Decathlon",
+    image: projectImage2,
+    description: `Decathlon is a global leader in sports equipment and apparel, offering a wide range of high-quality, affordable products for over 70 sports. Our mission is to make the pleasure and benefits of sports accessible to everyone. From beginners to professional athletes...`,
+    techstack: "HTML/CSS, Bootstrap",
+    previewLink: "https://cw-project-git-main-sagar-prasads-projects.vercel.app/",
+    githubLink: "https://github.com/Sgrprsd11704088/CW-Project",
+  },
+  {
+    title: "Viatours",
+    image: projectImage3,
+    description: `Viator is a leading online platform for booking tours, activities, and experiences worldwide. With a vast selection of curated options, Viator helps travelers discover and book memorable experiences tailored to their interests and destinations.`,
+    techstack: "HTML/CSS, Figma",
+    previewLink: "https://cw-project-nlti-git-main-sagar-prasads-projects.vercel.app/",
+    githubLink: "https://github.com/Sgrprsd11704088/CW-Project",
+  },
+  {
+    title: "No-Broker",
+    image: projectImage4,
+    description: `NoBroker is a pioneering online platform that eliminates middlemen by connecting property owners directly with tenants and buyers. With a mission to make real estate transactions simpler and cost-effective, NoBroker offers a hassle-free solution for renting, buying, and selling properties without brokerage fees.`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://660e695c7751b065e3797062--imaginative-pixie-6e83c9.netlify.app/",
+    githubLink: "https://github.com/Sartik0709/Compiler-Chintan-2345",
+  },
+];
+
+// Contact Details 
+export const contactDetails = {
+  email: "ps4059269@gmail.com",
+  phone: "+91 7985139275",
+};
